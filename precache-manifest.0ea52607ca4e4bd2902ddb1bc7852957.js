3d536974d6bfe196fec53e7757eeb96c502297a7
Mitox95 <maciek@kienig.pl>
2019-11-01 22:28:04 +0100

    Updates

---
+++ precache-manifest.0ea52607ca4e4bd2902ddb1bc7852957.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fec88ff41dce8db139756864c0fabfbc",
+    "url": "/rozklad/index.html"
+  },
+  {
+    "revision": "74670ae765f1b911b6fc",
+    "url": "/rozklad/static/css/main.7948a896.chunk.css"
+  },
+  {
+    "revision": "af3c0c7eb803e353f8a6",
+    "url": "/rozklad/static/js/2.e2c6fce6.chunk.js"
+  },
+  {
+    "revision": "74670ae765f1b911b6fc",
+    "url": "/rozklad/static/js/main.2646a390.chunk.js"
+  },
+  {
+    "revision": "62d60caec051635117a1",
+    "url": "/rozklad/static/js/runtime-main.4aff4f02.js"
+  }
+]);
